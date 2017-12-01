cab185ebd9771d00f1f3bd968321304014b90ab4
James Sun <jamessun@fb.com>
2017-12-01 09:55:05 -0800

    Add benchmark for DecayCounter

---
+++ stats/src/test/java/io/airlift/stats/BenchmarkDecayCounter.java
@@ -0,0 +1,89 @@
+package io.airlift.stats;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Group;
+import org.openjdk.jmh.annotations.GroupThreads;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Fork(2)
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+public class BenchmarkDecayCounter
+{
+    private static final int NUMBER_OF_ENTRIES = 100_000;
+
+    @State(Scope.Benchmark)
+    public static class Counter
+    {
+        private ShardedDecayCounter counter;
+
+        @Setup
+        public void setup()
+        {
+            counter = new ShardedDecayCounter(ExponentialDecay.oneMinute());
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class Data
+    {
+        private long[] values;
+
+        @Setup
+        public void setup()
+        {
+            values = new long[NUMBER_OF_ENTRIES];
+            for (int i = 0; i < NUMBER_OF_ENTRIES; i++) {
+                // generate values from a large domain but not many distinct values
+                long value = Math.abs((long) (ThreadLocalRandom.current().nextGaussian() * 1_000_000_000));
+                values[i] = (value / 1_000_000) * 1_000_000;
+            }
+        }
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(NUMBER_OF_ENTRIES)
+    @Group("counter")
+    @GroupThreads(40)
+    public Object benchmarkAdd(Counter counter, Data data)
+    {
+        for (long value : data.values) {
+            counter.counter.add(value);
+        }
+        return counter.counter;
+    }
+
+    @Benchmark
+    @Group("counter")
+    @GroupThreads(5)
+    public Object benchmarkGet(Counter counter)
+    {
+        counter.counter.getCount();
+        return counter.counter.getRate();
+    }
+
+    public static void main(String[] args)
+            throws RunnerException
+    {
+        Options options = new OptionsBuilder()
+                .include(".*" + BenchmarkDecayCounter.class.getSimpleName() + ".*")
+                .build();
+
+        new Runner(options).run();
+    }
+}
